5897fabe95284843c088863db9f1360312c69bf6
Alexey <alexey@debian.pro>
2023-07-24 00:31:18 +0700

    1st was simplified. effort to universal func to be don on MON

--- src/01/main.sh
+++ src/01/main.sh
@@ -8,7 +8,7 @@ test $E_BADARGS -eq 65 && exit $E_BADARGS
 LOG_LOCATION="$1/file.log"
 touch $LOG_LOCATION
 # the first NAMING is recursively changed folder name and the 2nd
-#+is regpattern for first NAMING evolution. 
+#+is regpattern for first NAMING evolution.
+#+six arg is absoliute path at start and (o_O) created folder name
+#+inside recursy for deep diving
 create_one_time $NAMING $4 $2 $NAMING $FILENAME $1 $6
-
-# bash main.sh ttt 3 ab 3 cd.ef 99
