30692bd8592708f1d7ecc472f910b91bd93f2e56
Alexey <alexey@debian.pro>
2023-07-23 22:46:20 +0700

    disavowal of one universal func

--- src/01/main.sh
+++ src/01/main.sh
@@ -9,6 +9,6 @@ SCRIPT_DATE=$(date +"%d%m%y")
 NAME_WITH_DATE=$(echo $5 | sed 's/$/'_$SCRIPT_DATE'/')
 LOG_LOCATION="$1/file.log"
 touch $LOG_LOCATION
-create_one_time $NAMING $4 $NAME_WITH_DATE $2 $NAMING $FILENAME $1 $6 K 0
+create_one_time $NAMING $4 $NAME_WITH_DATE $2 $NAMING $FILENAME $1 $6
 
 # bash main.sh ttt 3 ab 3 cd.ef 99
