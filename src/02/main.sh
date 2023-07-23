3b95869fba6a828e2f76b159f4149c823cf70719
Alexey <alexey@debian.pro>
2023-07-23 23:50:50 +0700

    first script to be simpled. recursy of second script to be implemented. TO BE DONE ON MONDAY

--- src/02/main.sh
+++ src/02/main.sh
@@ -4,8 +4,6 @@ source conditions.sh
 source ../creation_func.sh
 
 test $E_BADARGS -eq 65 && exit $E_BADARGS
-SCRIPT_DATE=$(date +"%d%m%y")
-NAME_WITH_DATE=$(echo $2 | sed 's/$/'_$SCRIPT_DATE'/')
 LOG_LOCATION="/file.log"
 touch $LOG_LOCATION
 cd /
@@ -13,10 +11,8 @@ cd /
 LIST=$(ls -d */)
 for I in $LIST; do
     [[ "$I" =~ $MATCH_RESTRICTED_FOLDERS ]] && continue #condition for restricted folders
-    START_FILE_COUNT=$(echo $(( 1+$RANDOM%100 )) )
-    echo $I $START_FILE_COUNT $NAME_WITH_DATE 100 $1 $FILENAME $I $3 M 1; echo; echo;
-    create_one_time $I $START_FILE_COUNT $NAME_WITH_DATE 100 \
-        $1 $FILENAME $I $3 M 1
+    create_one_time_var_2 $I $1 $2 $3 $FILENAME
+    cd /
 done
 
 
--- src/creation_func.sh
+++ src/creation_func.sh
@@ -40,40 +40,37 @@ function create_one_time ()
 
 function create_one_time_var_2 ()
 {
-    cd $7
-    FOLDERNAME=$1
-    FILECOUNT=$2
-    NEWFILENAME=$3
-    START_FOLDER_COUNT=$4
-    REGNAMING=$5
-    REGFILENAME=$6
-    DIR_PATH=$7
+    cd $1
+
+    SCRIPT_DATE=$(date +"%d%m%y")
+    NAME_WITH_DATE=$(echo $3 | sed 's/$/'_$SCRIPT_DATE'/')
+    echo $NAME_WITH_DATE
+    FILECOUNT=$(echo $(( 1+$RANDOM%100 )) )
+    FILESIZE=$4
+
+    START_FOLDER_COUNT=100
+
+    REGNAMING=$2
+    REGFILENAME=$5
     FILE_FORMER=${REGFILENAME:0:1}
     FOLDER_FORMER=${REGNAMING:0:1}
-    ! [[ $10 -eq 0 ]] && FILECOUNT=echo $(( 1+$RANDOM%100 ))
 
+    FILECOUNT=$( echo $(( 1+$RANDOM%100 )) )
 
     for (( y=1; y<=$FILECOUNT; y++ )); do
         FREE_SPACE=$(df -B G / | awk '{print $4}' | sed -e 's/G//')
         FREE_SPACE=$(echo $FREE_SPACE | awk '{print $2}' | sed -n '$p')
         [[ $FREE_SPACE -lt 1 ]] && return #end condition for loop
-        truncate -s $8"$9" $NEWFILENAME #here to be passed K or M
-        EXISTING=$(echo $PWD)
-        FOR_LOG=$(echo $NEWFILENAME | sed -e 's/$/'_$8'/')
-        FOR_LOG=$(echo | awk -v ONE=$NEWFILENAME -v TWO=$EXISTING '{print ONE"_"TWO}')
-        echo $FOR_LOG >> $LOG_LOCATION
-        NEWFILENAME=$(echo $NEWFILENAME | \
+        truncate -s $FILESIZE"M" $NAME_WITH_DATE
+        NAME_WITH_DATE=$(echo $NAME_WITH_DATE | \
             sed 's/'$FILE_FORMER'/'$FILE_FORMER''$FILE_FORMER'/')
     done
 
-    [[ $4 -eq 0 ]] && return #end condition for recursy
 
-    for (( y=1; y<=$START_FOLDER_COUNT; y++)); do
-        FOLDERNAME=$(echo $FOLDERNAME | \
-            sed 's/'$FOLDER_FORMER'/'$FOLDER_FORMER''$FOLDER_FORMER'/')
-        (( START_FOLDER_COUNT-- ))
-        mkdir $FOLDERNAME
-        create_one_time $FOLDERNAME $2 $NEWFILENAME \
-            $START_FOLDER_COUNT $5 $6 $FOLDERNAME $8 $9
-    done    
+
+    # [[ $4 -eq 0 ]] && return #end condition for recursy
+
+    # for (( y=1; y<=$START_FOLDER_COUNT; y++)); do
+
+    # done    
 }
