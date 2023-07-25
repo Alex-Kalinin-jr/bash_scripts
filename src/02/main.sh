68dc7fcef0afcc54c683a7a116c4a9976c396b8f
Alexey <alexey@debian.pro>
2023-07-25 23:39:04 +0700

    strange errors while descend into recursy number 2

--- src/02/main.sh
+++ src/02/main.sh
@@ -6,14 +6,8 @@ source ../creation_func.sh
 test $E_BADARGS -eq 65 && exit $E_BADARGS
 LOG_LOCATION="/file.log"
 touch $LOG_LOCATION
-cd /
-#single step
-LIST=$(ls -d */)
-for I in $LIST; do
-    [[ "$I" =~ $MATCH_RESTRICTED_FOLDERS ]] && continue #condition for restricted folders
-    create_one_time_var_2 $I $1 $2 $3 $FILENAME
-    cd /
-done
+create_many_times /
+
 
 
 #docker run -v $(pwd)/src:/home -it -w /home/02 cursebow/myubu
--- src/creation_func.sh
+++ src/creation_func.sh
@@ -41,41 +41,21 @@ function create_one_time ()
     done    
 }
 
-
-
-function create_one_time_var_2 ()
+function create_many_times ()
 {
     cd $1
-
-    SCRIPT_DATE=$(date +"%d%m%y")
-    NAME_WITH_DATE=$(echo $3 | sed 's/$/'_$SCRIPT_DATE'/')
-    echo $NAME_WITH_DATE
-    FILECOUNT=$(echo $(( 1+$RANDOM%100 )) )
-    FILESIZE=$4
-
-    START_FOLDER_COUNT=100
-
-    REGNAMING=$2
-    REGFILENAME=$5
-    FILE_FORMER=${REGFILENAME:0:1}
-    FOLDER_FORMER=${REGNAMING:0:1}
-
-    FILECOUNT=$( echo $(( 1+$RANDOM%100 )) )
-
-    for (( y=1; y<=$FILECOUNT; y++ )); do
-        FREE_SPACE=$(df -B G / | awk '{print $4}' | sed -e 's/G//')
-        FREE_SPACE=$(echo $FREE_SPACE | awk '{print $2}' | sed -n '$p')
-        [[ $FREE_SPACE -lt 1 ]] && return #end condition for loop
-        truncate -s $FILESIZE"M" $NAME_WITH_DATE
-        NAME_WITH_DATE=$(echo $NAME_WITH_DATE | \
-            sed 's/'$FILE_FORMER'/'$FILE_FORMER''$FILE_FORMER'/')
-    done
-
-
-
-    # [[ $4 -eq 0 ]] && return #end condition for recursy
-
-    # for (( y=1; y<=$START_FOLDER_COUNT; y++)); do
-
-    # done    
+    LIST=$(ls -d */)
+    if ! [[ $(echo $?) -eq 2 ]]
+    then 
+        for I in $LIST; do
+            CUR_FOLDER=$(echo $PWD)
+            [[ "$I" =~ $MATCH_RESTRICTED_FOLDERS ]] && continue
+            F_COUNT=$((1+$RANDOM%100))
+            NEW_FOLDER=$(echo | awk -v ONE=$CUR_FOLDER -v TWO=$I '{print ONE"/"TWO}')
+            create_many_times $NEW_FOLDER
+            create_one_time $NAMING $F_COUNT 2 $NAMING $FILENAME $I $3
+            cd $CUR_FOLDER
+        done
+    fi
+    
 }
