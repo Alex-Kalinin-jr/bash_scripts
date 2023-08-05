88c7771cbb31334f16fbd45c903a780c1b32eb85
alexey <alexey@alexey.alexey>
2023-08-05 18:06:19 +0800

    zdes toje budet "nobody do that better"

--- src/04/main.sh
+++ src/04/main.sh
@@ -6,9 +6,9 @@ source values_generator.sh
 URLS_FILE="sites_list.txt"
 AGENTS_FILE="user_agents.txt"
 
-# while [[  ]]
 NEW_IP=$(printf "%d.%d.%d.%d\n" \
-    "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))")
+    "$((RANDOM % 256))" "$((RANDOM % 256))" \
+    "$((RANDOM % 256))" "$((RANDOM % 256))")
 NEW_TIME=$(ran_time)
 NEW_CODE=$(ran_code)
 NEW_PROTO=$(ran_proto)
@@ -16,4 +16,6 @@ NEW_METHOD=$(ran_method)
 NEW_URL=$(ran_line ${URLS_FILE})
 NEW_AGENT=$(ran_line ${AGENTS_FILE})
 
-echo "${NEW_IP} - ${NEW_CODE} - ${NEW_METHOD} - ${NEW_TIME} - ${NEW_PROTO} - ${NEW_URL} - ${NEW_AGENT}"
+printf "%s - %s - %s - %s - %s - %s - %s\n" \
+        "${NEW_IP}" "${NEW_CODE}" "${NEW_METHOD}" \
+        "${NEW_TIME}" "${NEW_PROTO}" "${NEW_URL}" "${NEW_AGENT}"
